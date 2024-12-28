b1a7ae17dc1961c9e14a58545dceec97d82412ff
Gianfranco Zamboni <gzamboni@dc.uba.ar>
2024-12-28 20:14:36 -0300

    Enable shadows

--- src/exercises/02-classic-techniques/01-lights.js
+++ src/exercises/02-classic-techniques/01-lights.js
@@ -1,213 +1,6 @@
 import GUI from 'lil-gui';
 import * as THREE from 'three';
-import { AnimationLoop } from '../../utils/animation-loop';
-import { RectAreaLightHelper } from 'three/examples/jsm/Addons.js';
-
-const MIN_MAX = {
-  angle: { min: 0, max: Math.PI / 2, step: 0.01 },
-  decay: { min: 0, max: 2, step: 0.01 },
-  distance: { min: 0, max: 1, step: 0.001 },
-  height: { min: 0, max: 1, step: 0.01 },
-  intensity: { min: 0, max: 6, step: 0.01 },
-  penumbra: { min: 0, max: 1, step: 0.01 },
-  width: { min: 0, max: 1, step: 0.01 },
-  x: { min: -5, max: 5, step: 0.01 },
-  y: { min: -5, max: 5, step: 0.01 },
-  z: { min: -5, max: 5, step: 0.01 },
-}
-
-class CustomLight {
-  constructor(type, params, config = {}) {
-    this.type = type;
-    this.on = true;
-    this.params = params;
-    this.config = config;
-    this.lightControls = [];
-
-    this.light = new THREE[type](...Object.values(this.params));
-
-    if (this.config.position) {
-      this.setPosition(this.config.position);
-    }
-
-    if (this.config.lookAt) {
-      this.setLookAt(this.config.lookAt);
-    }
-
-    if (this.config.targetPosition) {
-      this.setTargetPosition(this.config.targetPosition);
-    }
-
-    this.helper = null;
-
-    switch (type) {
-      case "DirectionalLight":
-        this.helper = new THREE.DirectionalLightHelper(this.light, 0.2);
-        break;
-      case "HemiSphereLight":
-        this.helper = new THREE.HemisphereLightHelper(this.light, 0.2);
-        break;
-      case "PointLight":
-        this.helper = new THREE.PointLightHelper(this.light, 0.2);
-        break;
-      case "SpotLight":
-        this.helper = new THREE.SpotLightHelper(this.light);
-        break;
-      case "RectAreaLight":
-        this.helper = new RectAreaLightHelper(this.light);
-        break;
-      default:
-    }
-  }
-
-  setPosition(newPosition) {
-    this.light.position.x = newPosition.x;
-    this.light.position.y = newPosition.y;
-    this.light.position.z = newPosition.z;
-  }
-
-  setLookAt(newLookAt) {
-    const newTarget = new THREE.Vector3(
-      newLookAt.x,
-      newLookAt.y,
-      newLookAt.z
-    );
-    this.light.lookAt(newTarget);
-  }
-
-  setTargetPosition(newPosition) {
-    this.light.target.position.x = newPosition.x;
-    this.light.target.position.y = newPosition.y;
-    this.light.target.position.z = newPosition.z;
-  }
-
-  addControls(gui) {
-    this.lightSettingFolder = gui.addFolder(this.type);
-    this.createOnOffControl();
-    this.createLightParamsControls();
-    this.createExtraControls();
-  }
-
-  createPositionalControl(param, onChange) {
-    const positionFolder = this.lightSettingFolder.addFolder(param);
-    positionFolder.close();
-    ["x", "y", "z"].forEach((axis) => {
-      const minMax = MIN_MAX[axis];
-
-      const control = positionFolder
-        .add(this.config[param], axis)
-        .min(minMax.min)
-        .max(minMax.max)
-        .step(minMax.step)
-        .name(axis)
-        .onChange(() => {
-          onChange(axis);
-        });
-
-      this.lightControls.push(control);
-    });
-  }
-
-  createExtraControls() {
-    if (this.config.position !== undefined) {
-      this.createPositionalControl('position', (axis) => {
-        this.light.position[axis] = this.config.position[axis];
-      });
-    }
-
-    if (this.config.lookAt !== undefined) {
-      this.createPositionalControl('lookAt', () => {
-        const newTarget = new THREE.Vector3(
-          this.config.lookAt.x,
-          this.config.lookAt.y,
-          this.config.lookAt.z
-        );
-        this.light.lookAt(newTarget);
-      });
-    }
-
-    if (this.config.targetPosition !== undefined) {
-      this.createPositionalControl('targetPosition', (axis) => {
-        this.light.target.position[axis] = this.config.targetPosition[axis];
-      });
-    }
-    
-  }
-  createOnOffControl() {
-    this.lightSettingFolder.add(this, 'on').name('On/Off').onChange(() => {
-      this.light.visible = this.on;
-      this.lightControls.forEach(control => {
-        if(this.helper) {
-          this.helper.visible = this.on;
-        }
-        
-        if(this.on === false) {
-         control.disable();
-        } else {
-          control.enable();
-        }
-      });
-    });
-  }
-
-  createColorControl(param) {
-    const control = this.lightSettingFolder
-      .addColor(this.params, param)
-      .name(param)
-      .onChange(() => {
-        this.light[param].set(this.params[param]);
-      });
-
-    this.lightControls.push(control);
-  }
-
-  createNumericControl(param) {
-    const minMax = MIN_MAX[param];
-    const control = this.lightSettingFolder
-      .add(this.params, param)
-      .min(minMax.min)
-      .max(minMax.max)
-      .step(minMax.step)
-      .name(param)
-      .onChange(() => {
-        this.light[param] = this.params[param];
-    });
-    this.lightControls.push(control);
-  }
-
-  createLightParamsControls() {
-    const params = this.params;
-    Object.keys(params).forEach(param => {
-      if (typeof params[param] === "string") {
-        this.createColorControl(param);
-      } else {
-        this.createNumericControl(param);
-      }
-    });
-  }
-
-  addTo(scene) {
-    scene.add(this.light);
-    if (this.helper) {
-      scene.add(this.helper);
-    }
-  }
-
-  dispose(scene) {
-    if (this.helper) {
-      scene.remove(this.helper);
-      this.helper.dispose();
-    }
-
-    if(this.light.target) {
-      scene.remove(this.light.target);
-      this.light.target.clear();
-    }
-
-    scene.remove(this.light);
-    this.light.dispose();
-  }
-}
+import { CustomizableLight } from '../../utils/customizable-light';
 
 export class LightsExercise {
   constructor(view) {
@@ -216,39 +9,44 @@ export class LightsExercise {
     this.scene = new THREE.Scene();
     this.clock = new THREE.Clock();
     this.lights = [
-      new CustomLight("AmbientLight", {
+      new CustomizableLight("AmbientLight", {
         color: "#ffffff",
         intensity: 0.2,
       }),
-      new CustomLight("DirectionalLight", {
+      new CustomizableLight("DirectionalLight", {
         color: "#00fffc",
         intensity: 0.9,
       },{
-        position: { x: 1, y: 0, z: 0}
+        position: { x: 1, y: 0, z: 0},
+        showHelper: true
       }),
-      new CustomLight("HemisphereLight", {
+      new CustomizableLight("HemisphereLight", {
         color: "#ff0000",
         groundColor: "#0000ff",
         intensity: 0.9,
+      },{
+        showHelper: true
       }),
-      new CustomLight("PointLight", {
+      new CustomizableLight("PointLight", {
         color: "#ff9000",
         intensity: 1.5,
         distance: 0.5,
         decay: 2,
       },{
         position: { x: 1, y: -0.5, z: 1 },
+        showHelper: true
       }),
-      new CustomLight("RectAreaLight", {
+      new CustomizableLight("RectAreaLight", {
         color: "#4e00ff",
         intensity: 6,
         width: 1,
         height: 1,
       },{
         position: { x: -1.5, y:  0, z: 1.5},
-        lookAt: { x: 0, y: 0, z: 0 }
+        lookAt: { x: 0, y: 0, z: 0 },
+        showHelper: true
       }),
-      new CustomLight("SpotLight", {
+      new CustomizableLight("SpotLight", {
         color: "#78ff00",
         intensity: 4.5,
         distance: 0,
@@ -257,7 +55,8 @@ export class LightsExercise {
         decay: 1,
       },{
         position: { x:0, y: 2, z: 3},
-        targetPosition: { x: -0.75, y: 0, z: 0 }
+        targetPosition: { x: -0.75, y: 0, z: 0 },
+        showHelper: true
       })
     ]
 
@@ -301,12 +100,7 @@ export class LightsExercise {
 
   animationFrame() {
     const elapsedTime = this.clock.getElapsedTime();
-    this.lights.forEach(light => {
-      if(light.type === "SpotLight") {
-        light.helper.update();
-      }
-    });
-
+    this.lights.forEach(light => light.update());
     this.objects.forEach(object => {
       object.rotation.y = 0.1 * elapsedTime;
       object.rotation.x = 0.15 * elapsedTime;
--- src/exercises/02-classic-techniques/02-shadow.js
+++ src/exercises/02-classic-techniques/02-shadow.js
@@ -1,5 +1,6 @@
 import GUI from 'lil-gui';
 import * as THREE from 'three';
+import { CustomizableLight } from '../../utils/customizable-light';
 
 export class ShadowExercise {
   constructor(view) {
@@ -7,8 +8,21 @@ export class ShadowExercise {
     this.scene = new THREE.Scene();
     this.gui = new GUI();
 
-    this.ambientLight = new THREE.AmbientLight(0xffffff, 1);
-    this.directionalLight = new THREE.DirectionalLight(0xffffff, 1.5);
+    this.lights = {
+      ambient: new CustomizableLight('AmbientLight', {
+        color: "#ff0000",
+        intensity: 1
+      }),
+      directional: new CustomizableLight('DirectionalLight', {
+        color: "#00ff00",
+        intensity: 1.5
+      }, {
+        position: { x: 2, y: 2, z: -1 },
+      }),
+    };
+
+    this.spotLight = new THREE.SpotLight(0x0000ff, 3.6, 10, Math.PI * 0.3);
+    this.pointLight = new THREE.PointLight(0x0ff000, 2.7);
     this.material = new THREE.MeshStandardMaterial();
 
     this.sphere = new THREE.Mesh(
@@ -23,9 +37,38 @@ export class ShadowExercise {
   }
   
   init() {
-    this.directionalLight.position.set(2, 2, -1);
-    this.scene.add(this.directionalLight);
-    this.scene.add(this.ambientLight);
+    Object.values(this.lights).forEach((light) => {
+      light.addTo(this.scene);
+      light.addControls(this.gui);
+    });
+
+    this.spotLight.position.set(0, 2, 2);
+    this.pointLight.position.set(-1, 1, 0);
+    
+    ["directional", ].forEach((lightType) => {
+      this.lights[lightType].enableShadows();
+    });
+    
+    [this.spotLight].forEach((light) => {
+      light.castShadow = true;
+      light.shadow.mapSize.width = 1024;
+      light.shadow.mapSize.height = 1024;
+      light.shadow.camera.near = 1;
+      light.shadow.camera.far = 6;
+      //this.directionalLight.shadow.radius = 10;
+    });
+    
+    this.lights.directional.shadow.camera.top = 2;
+    this.lights.directional.shadow.camera.right = 2;
+    this.lights.directional.shadow.camera.bottom = -2;
+    this.lights.directional.shadow.camera.left = -2;
+
+    this.directionalCameraHelper = new THREE.CameraHelper(this.lights.directional.shadow.camera);
+    this.directionalCameraHelper.visible = false;
+
+    this.spotLightCameraHelper = new THREE.CameraHelper(this.spotLight.shadow.camera);
+    this.spotLightCameraHelper.visible = false;
+
     this.addLightControls();
 
     this.material.roughness = 0.7;
@@ -33,24 +76,45 @@ export class ShadowExercise {
 
     this.plane.rotation.x = -Math.PI * 0.5;
     this.plane.position.y = -0.5;
-    this.scene.add(this.sphere, this.plane);
+
+    this.sphere.castShadow = true;
+    this.plane.receiveShadow = true;
+
+    this.scene.add(
+      this.spotLight,
+      this.spotLight.target,
+      this.directionalCameraHelper,
+      this.spotLightCameraHelper,
+      this.pointLight,
+      this.sphere, 
+      this.plane
+    );
 
     this.view.setCamera({
       position: { x: 1, y: 1, z: 2 },
       lookAt: { x: 0, y: 0, z: 0 }
     });
+
+    this.view.enableShadows()
     this.view.toggleOrbitControls(true);
     this.view.show(this.scene);
   }
 
   dispose() {
     this.gui.destroy();
+    Object.values(this.lights).forEach((light) => {
+      light.dispose(this.scene);
+    });
+
     [this.sphere, this.plane].forEach((object) => {
       this.scene.remove(object);
       object.geometry.dispose();
     });
     this.material.dispose();
-    [this.ambientLight, this.directionalLight].forEach((light) => { 
+    this.directionalCameraHelper.dispose();
+    this.spotLightCameraHelper.dispose();
+    this.pointLight.dispose();
+    [this.spotLight].forEach((light) => { 
       this.scene.remove(light);
       light.dispose();
     });
@@ -67,24 +131,23 @@ export class ShadowExercise {
   }
 
   addLightControls() {
-    const folder = this.gui.addFolder('Ambient Light');
-    folder.add(this.ambientLight, 'intensity')
-      .min(0)
-      .max(3)
-      .step(0.001);
-
-    const directionalLightFolder = this.gui.addFolder('Directional Light');
-    directionalLightFolder.add(this.directionalLight, 'intensity')
-      .min(0)
-      .max(3)
-      .step(0.001);
-
-    const positionFolder = directionalLightFolder.addFolder('Position');
-    ['x', 'y', 'z'].forEach((axis) => {
-      positionFolder.add(this.directionalLight.position, axis)
-        .min(-5)
-        .max(5)
+    [this.spotLight, this.pointLight].forEach((light) => {
+      const folder = this.gui.addFolder(light.type);
+      folder.add(light, 'intensity')
+        .min(0)
+        .max(3)
         .step(0.001);
+
+      if (light.type === 'SpotLight' || light.type === 'PointLight') {
+        const positionFolder = folder.addFolder('Position');
+        
+        ['x', 'y', 'z'].forEach((axis) => {
+          positionFolder.add(light.position, axis)
+            .min(-5)
+            .max(5)
+            .step(0.001);
+        });
+      }
     });
   }
 }
