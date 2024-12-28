204df21dae98dff1f904ed512615e7f424ec27b8
Gianfranco Zamboni <gzamboni@dc.uba.ar>
2024-12-28 12:37:16 -0300

    History save (#4)

--- src/script.js
+++ src/script.js
@@ -1,14 +1,33 @@
 import { BasicView } from './app/basic-view.js';
-import { Menu } from './app/gui.js';
+import { Menu } from './app/menu.js';
 import { journey } from './app/journey.js';
 
-const lastChapterExercises = journey[journey.length - 1].exercises;
-const lastExercise = lastChapterExercises[lastChapterExercises.length - 1];
+function selectExercise() {
+  const url = new URL(window.location.href);
+  const exerciseId = url.pathname.slice(1);
+  if(exerciseId === '') {
+    const lastChapterExercises = journey[journey.length - 1].exercises;
+    return lastChapterExercises[lastChapterExercises.length - 1];
+  } else {
+    return journey.map((chapter) => chapter.exercises)
+      .flat()
+      .find((exercise) => exercise.id === exerciseId);
+  }
+}
+
+const initialExercise = selectExercise();
 
 const view = new BasicView();
-const menu = new Menu(lastExercise, async (exercise) => {
+const menu = new Menu(initialExercise, async (exercise) => {
   console.log(`Loading exercise: ${exercise.title}`);
+  history.pushState(exercise.id, "", exercise.id);
   await view.run(exercise);
 });
 
-view.run(lastExercise);
+view.run(initialExercise);
+
+window.addEventListener('popstate', async (event) => {
+  const exercise = selectExercise();
+  console.log(`Loading exercise: ${exercise.title}`);
+  await view.run(exercise);
+});
