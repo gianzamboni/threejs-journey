baa22d9b999a86cc3d986c40b55d6c98d9afa092
Gianfranco Zamboni <gzamboni@dc.uba.ar>
2025-01-06 21:22:34 -0300

    Different colors

--- src/exercises/02-classic-techniques/05-particles.js
+++ src/exercises/02-classic-techniques/05-particles.js
@@ -8,27 +8,31 @@ export class Particles {
     this.scene = new THREE.Scene();
 
     this.particleGeometry = new THREE.BufferGeometry();
-    const count = 500000;
+    const count = 50000;
 
     const positions = new Float32Array(count * 3);
+    const colors = new Float32Array(count * 3);
     for (let i = 0; i < count * 3; i++) {
       positions[i] = (Math.random() - 0.5) * 10;
+      colors[i] = Math.random();
     }
 
     this.particleGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+    this.particleGeometry.setAttribute('color', new THREE.BufferAttribute(colors, 3));
 
     this.particleTexture = TEXTURE_LOADER.load('/textures/particles/2.png');
 
     this.pointMaterial = new THREE.PointsMaterial({
-      size: 0.025,
+      size: 0.1,
       sizeAttenuation: true,
       transparent: true,
-      color: new THREE.Color(0xff88cc),
+      //color: new THREE.Color(0xff88cc),
       alphaMap: this.particleTexture,
       //alphaTest: 0.001,
       //depthTest: false,
       depthWrite: false,
       blending: THREE.AdditiveBlending,
+      vertexColors: true,
     });
 
     this.particles = new THREE.Points(this.particleGeometry, this.pointMaterial);
@@ -36,7 +40,7 @@ export class Particles {
     this.view.setOrbitControlSettings({
       zoomSpeed: 0.1,
       autoRotate: true,
-      autoRotateSpeed: 0.02,
+      autoRotateSpeed: 0.03,
     })
 
     this.view.setCamera({
