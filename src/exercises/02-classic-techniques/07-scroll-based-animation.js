c90218c7220299a370ced6b866f3a84a3883a3ff
Gianfranco Zamboni <gzamboni@dc.uba.ar>
2025-01-09 14:50:34 -0300

    Final scroll based animation

--- src/exercises/02-classic-techniques/07-scroll-based-animation.js
+++ src/exercises/02-classic-techniques/07-scroll-based-animation.js
@@ -1,6 +1,7 @@
 import * as THREE from 'three'
 import GUI from 'lil-gui';
 import { TEXTURE_LOADER } from '../../utils/loading-manager';
+import gsap from 'gsap';
 
 export class ScrollBasedAnimation {
   constructor(view, debugUI) {
@@ -58,34 +59,48 @@ export class ScrollBasedAnimation {
 
     this.scrollPostion = window.scrollY;
     this.cursorPosition = { x: 0, y: 0 };
+    this.currentSection = 0;
     this.directionaLight = new THREE.DirectionalLight(0xffffff, 3);
     this.cameraGroup = new THREE.Group();
+
+    const particleCount = 200;
+    const positions = new Float32Array(particleCount * 3);
+    
+    for(let i = 0; i < particleCount; i++) {
+      positions[i * 3] = (Math.random() - 0.5)* 10;
+      positions[i * 3 + 1] = this.settings.objectDistance - Math.random() * this.settings.objectDistance * this.htmlSections.length;
+      positions[i * 3 + 2] = (Math.random() - 0.5)* 10;
+    }
+
+    this.particlesGeometry = new THREE.BufferGeometry();
+    this.particlesGeometry.setAttribute('position', new THREE.BufferAttribute(positions, 3));
+    this.particlesMaterial = new THREE.PointsMaterial({
+      color: this.settings.materialColor,
+      sizeAttenuation: true,
+      size: 0.03
+    });
+
+    this.particles = new THREE.Points(this.particlesGeometry, this.particlesMaterial);
   }
 
   async init() {
     this.view.setClearAlpha(0);
 
     this.view.setCamera({
-      position: {
-        x: 0,
-        y: 0,
-        z: 6
-      },
-      lookAt: {
-        x: 0,
-        y: 0,
-        z: 0
-      },
+      position: { x: 0, y: 0, z: 6 },
+      lookAt: { x: 0, y: 0, z: 0 },
       fov: 35,
     })
     
     this.meshes.forEach((mesh, index) => {
       mesh.position.y = - this.settings.objectDistance * index;
     })
+    this.scene.add(this.particles);
 
     this.directionaLight.position.set(1, 0, 0);
     this.gui.addColor(this.settings, 'materialColor').name('Material Color').onChange((event) => {
       this.material.color.set(new THREE.Color(this.settings.materialColor));
+      this.particlesMaterial.color.set(new THREE.Color(this.settings.materialColor));
     });
 
     this.scene.add(...this.meshes, this.directionaLight, this.cameraGroup);
@@ -93,12 +108,11 @@ export class ScrollBasedAnimation {
   }
 
   animation(timer) {
-    const elapsedTime = timer.getElapsed();
     const deltaTime = timer.getDelta();
 
     this.meshes.forEach(mesh => {
-      mesh.rotation.x = elapsedTime * 0.1;
-      mesh.rotation.y = elapsedTime * 0.12;
+      mesh.rotation.x += deltaTime * 0.1;
+      mesh.rotation.y += deltaTime * 0.12;
     })
 
     this.view.camera.position.y = -this.scrollPostion / this.view.size.height * this.settings.objectDistance;
@@ -114,6 +128,18 @@ export class ScrollBasedAnimation {
 
   scroll(value) {
     this.scrollPostion = value;
+
+    const newSection = Math.round(value / this.view.size.height);
+    if(newSection !== this.currentSection) {
+      this.currentSection = newSection;
+      gsap.to(this.meshes[this.currentSection].rotation, {
+        duration: 1.5,
+        ease: 'power2.inOut',
+        x: "+=6",
+        y: "+=3",
+        z: "+=1.5"
+      })
+    };
   }
 
   mouseMove(x, y) {
@@ -128,10 +154,15 @@ export class ScrollBasedAnimation {
     this.htmlSections.forEach((section) => {
       section.remove();
     });
-    this.scene.remove(this.cube);
+
+    this.scene.remove(this.particles);
     this.meshes.forEach((mesh) => {
+      this.scene.remove(mesh);
       mesh.geometry.dispose();
     });
+
+    this.bufferGeometry.dispose();
+    this.particlesMaterial.dispose();
     this.material.dispose();
     this.gradientTexture.dispose();
     document.body.style.overflow = 'auto';
