0e10fd5a07c7eec9d585018fe53304d144d5a8e4
Gianfranco Zamboni <gianfraco376@gmail.com>
2025-02-10 12:45:22 -0300

    Texture test

--- src/app/journey/01-basics/07-textures.ts
+++ src/app/journey/01-basics/07-textures.ts
@@ -20,7 +20,7 @@ export class TextureTest extends OrbitControlledExercise {
     this.minecraftTexture = this.loadMinecrafTexture();
     console.log(this.minecraftTexture);
     this.geometry = new THREE.BoxGeometry(1, 1, 1, 1, 1, 1);
-    this.material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+    this.material = new THREE.MeshBasicMaterial({ map: this.minecraftTexture });
     this.cube = new THREE.Mesh(this.geometry, this.material);
     console.log(this.cube);
     this.camera.position.set(2, 2, 2);
@@ -45,51 +45,3 @@ export class TextureTest extends OrbitControlledExercise {
     this.minecraftTexture.dispose();
   }
 }
-
-/*
-
-import * as THREE from 'three'
-import { TEXTURE_LOADER } from '../../utils/loading-manager';
-import { dispose } from '../../utils/dispose';
-export class TextureExercise {
-  constructor(view) {
-    this.view = view;
-    this.scene = new THREE.Scene();
-    this.colorTexture = this.loadMinecrafTexture();
-    this.geometry = new THREE.BoxGeometry(1, 1, 1, 1, 1, 1);
-    this.material = new THREE.MeshBasicMaterial({ map: this.colorTexture });
-    this.mesh = new THREE.Mesh(this.geometry, this.material);
-  }
-
-  init() {
-    this.scene.add(this.mesh);
-    this.view.setCamera({
-      position: { x: 2, y: 2, z: 2 },
-      lookAt: { x: 0, y: 0, z: 0 }
-    });
-    this.view.setOrbitControlSettings({
-      autoRotate: true,
-      autoRotateSpeed: 1,
-    });
-    this.view.show(this.scene);
-  }
-
-  loadMinecrafTexture() {
-    const colorTexture = TEXTURE_LOADER.load('/textures/minecraft/minecraft.png');
-    colorTexture.colorSpace = THREE.SRGBColorSpace;
-    colorTexture.wrapS = THREE.RepeatWrapping;
-    colorTexture.wrapT = THREE.RepeatWrapping;
-    colorTexture.generateMipmaps = false;
-    colorTexture.minFilter = THREE.NearestFilter;
-    colorTexture.magFilter = THREE.NearestFilter;
-    return colorTexture;
-  }
-
-  async dispose() {
-    this.scene.remove(this.mesh);
-    dispose(this.mesh);
-    this.colorTexture.dispose();
-  }
-}
-
-*/
