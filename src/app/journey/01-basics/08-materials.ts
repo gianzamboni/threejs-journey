17a6ae5399ef61a8614a7f38354847a800fd6518
Gianfranco Zamboni <gzamboni@dc.uba.ar>
2025-02-10 23:15:41 -0300

    WIP: RGBE Loader

--- src/app/journey/01-basics/08-materials.ts
+++ src/app/journey/01-basics/08-materials.ts
@@ -1,29 +1,64 @@
 import * as THREE from 'three';
 import OrbitControlledExercise from '../exercises/orbit-controlled-exercise';
 import RenderView from '@/app/layout/render-view';
-import { createRedCube } from '@/app/utils/default-shapes';
 import { DebugFPS, Debuggable } from '../exercises/debug-decorators';
+import { AssetLoader } from '@/app/utils/assets-loader';
 
 @Debuggable
 export class MaterialsTest extends OrbitControlledExercise {
 
   public static id = 'materials';
   
-  private cube: THREE.Mesh;
+  private physicalMaterial: THREE.MeshPhysicalMaterial;//THREE.MeshPhysicalMaterial;
+  private geometries: THREE.BufferGeometry[];
+  private meshes: THREE.Mesh[];
+  private envMap: THREE.Texture;
+  private loader: AssetLoader;
 
   constructor(view: RenderView) {
     super(view);
-    this.cube = createRedCube();
-    this.scene.add(this.cube);
+    this.loader = AssetLoader.getInstance();
+    this.setupEnvironment();
+
+    this.physicalMaterial = this.createMaterial();
+
+    this.geometries = [
+      new THREE.SphereGeometry(0.5, 64, 64),
+      new THREE.BoxGeometry(1, 1, 1, 100, 100),
+      new THREE.ConeGeometry(0.3, 0.2, 64, 128),
+    ];
+
+    this.meshes = this.geometries.map(g => new THREE.Mesh(g, this.physicalMaterial));
+    this.scene.add(...this.meshes);
   }
 
   @DebugFPS
   frame() {
     super.frame();
   }
+
+  private setupEnvironment() {
+    this.loader.loadEnvironment('envMaps/alley_2k.hdr', (envMap) => {
+      envMap.mapping = THREE.EquirectangularReflectionMapping;
+      this.scene.environment = envMap;
+      this.scene.background = envMap;
+      this.envMap = envMap;   
+    });
+  }
+
+  private createMaterial() {
+    return new THREE.MeshPhysicalMaterial({
+      metalness: 0,
+      roughness: 0.5,
+      transmission: 1,
+      ior: 1.5,
+      thickness: 0.5,
+      side: THREE.DoubleSide,
+    });
+  }
   async dispose() {
     super.dispose();
-    this.cube.geometry.dispose();
-    (this.cube.material as THREE.Material).dispose();
+    this.geometries.forEach(g => g.dispose());
+    this.physicalMaterial.dispose();
   }
 }
--- src/app/utils/assets-loader.ts
+++ src/app/utils/assets-loader.ts
@@ -1,4 +1,5 @@
 import * as THREE from 'three';
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js';
 
 export type LoadingData = {
   url: string;
@@ -11,7 +12,9 @@ export class AssetLoader extends EventTarget{
   private static instance: AssetLoader | undefined;
 
   private loadingManager: THREE.LoadingManager;
+
   private textureLoader: THREE.TextureLoader;
+  private rgbeLoader: RGBELoader;
 
   public static getInstance() {
     if (!AssetLoader.instance) {
@@ -29,6 +32,7 @@ export class AssetLoader extends EventTarget{
     this.loadingManager.onLoad = this.onLoad.bind(this);
 
     this.textureLoader = new THREE.TextureLoader(this.loadingManager);
+    this.rgbeLoader = new RGBELoader(this.loadingManager);
   }
 
   onStart(url: string, itemsLoaded: number, itemsTotal: number) {
@@ -59,5 +63,9 @@ export class AssetLoader extends EventTarget{
 
   loadTexture(url: string) {
     return this.textureLoader.load(url);
+  }
+
+  loadEnvironment(url: string, onLoad: (texture: THREE.Texture) => void) {
+    return this.rgbeLoader.load(url, onLoad);
   }
 }
