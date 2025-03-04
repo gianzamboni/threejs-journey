8b45d098da42cdcb5dc610febadec20de99424be
Gianfranco Zamboni <gianfraco376@gmail.com>
2025-03-03 23:35:29 -0300

    Physics exercise done

--- src/app/journey/03-advanced-techniques/01-physics.ts
+++ src/app/journey/03-advanced-techniques/01-physics.ts
@@ -1,17 +1,246 @@
+import * as CANNON from 'cannon-es';
+import * as THREE from 'three';
+
+import { Timer } from 'three/addons/misc/Timer.js';
+
+import { Callable } from '#/app/decorators/customizable';
+import { DebugFPS } from '#/app/decorators/debug';
 import { Exercise } from "#/app/decorators/exercise";
 import RenderView from "#/app/layout/render-view";
+import { Position3D } from '#/app/types/exercise';
+import { AssetLoader } from "#/app/utils/assets-loader";
 
 import OrbitControlledExercise from "../exercises/orbit-controlled-exercise";
+
+type PhysicalObject = {
+  mesh: THREE.Mesh;
+  physics: CANNON.Body;
+}
 
 @Exercise('physics')
 export class Physics extends OrbitControlledExercise {
 
+  private environmentMap: THREE.Texture;
+
+  private material: THREE.MeshStandardMaterial;
+
+  private sphereGeometry: THREE.SphereGeometry;
+  private boxGeometry: THREE.BoxGeometry;
+
+  private physicalObjects: PhysicalObject[];
+
+  private floor: PhysicalObject;
+
+  private ambientLight: THREE.AmbientLight;
+  private directionalLight: THREE.DirectionalLight;
+
+  private physicsWorld: CANNON.World;
+
+  private hitSound: HTMLAudioElement;
+
   constructor(view: RenderView) {
     super(view);
+    view.enableShadows(THREE.PCFSoftShadowMap);
+
+    this.environmentMap = this.loadEnvironmentMap();
+
+    this.physicsWorld = this.setupPhysics();
+    
+    this.material = new THREE.MeshStandardMaterial({
+      metalness: 0.3,
+      roughness: 0.4,
+      envMap: this.environmentMap,
+      envMapIntensity: 0.5
+    });
+
+    this.sphereGeometry = new THREE.SphereGeometry(1, 32, 32);
+    this.boxGeometry = new THREE.BoxGeometry(1, 1, 1);
+    this.physicalObjects = [];
+
+    this.floor = this.createFloor();
+
+    this.ambientLight = new THREE.AmbientLight(0xffffff, 2.1);
+    this.directionalLight = this.createDirectionalLight();
+
+    this.camera.position.set(-3, 3, 3);
+
+    this.physicsWorld.addBody(this.floor.physics);
+    this.scene.add(this.floor.mesh, this.ambientLight, this.directionalLight);
+
+    this.hitSound = new Audio('sounds/hit.mp3');
   }
-  
+
+  @DebugFPS
+  frame(timer: Timer) {
+    super.frame(timer);
+    const delta = timer.getDelta();
+
+    this.physicsWorld.step(1 / 60, delta, 3);
+
+    for (const object of this.physicalObjects) {
+      object.mesh.position.copy(object.physics.position);
+      object.mesh.quaternion.copy(object.physics.quaternion);
+    }
+  }
+
+  public playHitSound(collision: { contact: CANNON.ContactEquation}) {
+    const velocity = collision.contact.getImpactVelocityAlongNormal();
+    console.log(velocity);
+    if (velocity > 1.5) {
+      this.hitSound.volume = Math.random();
+      this.hitSound.currentTime = 0;
+      this.hitSound.play();
+    }
+  }
+
+  private setupPhysics() {
+    const world = new CANNON.World();
+    world.gravity.set(0, -9.82, 0);
+    world.broadphase = new CANNON.SAPBroadphase(world);
+    world.allowSleep = true;
+
+    const material = new CANNON.Material('default');
+    const contactMaterial = new CANNON.ContactMaterial(material, material, {
+        friction: 0.1,
+        restitution: 0.7,
+    });
+
+    world.defaultContactMaterial = contactMaterial;
+    return world;
+  }
+
+  @Callable('', 'Add Sphere')
+  public addSphere() {
+    const radius = Math.random() * 0.5;
+    const position = this.getRandomPosition();
+    const sphere = this.createSphere(radius, position);
+    this.physicalObjects.push(sphere);
+  }
+
+  @Callable('', 'Add Box')
+  public addBox() {
+    const width = Math.random();
+    const height = Math.random();
+    const depth = Math.random();
+    const position = this.getRandomPosition();
+
+    const box = this.createBox(width, height, depth, position);
+    this.physicalObjects.push(box);
+  }
+
+  private getRandomPosition() {
+    return {
+      x: (Math.random() - 0.5) * 3,
+      y: 3,
+      z: (Math.random() - 0.5) * 3,
+    }
+  }
+
+  @Callable('', 'Remove all')
+  public clearScene() {
+    for (const object of this.physicalObjects) {
+      object.physics.removeEventListener('collide', this.playHitSound.bind(this));
+      this.physicsWorld.removeBody(object.physics);
+      this.scene.remove(object.mesh);
+    }
+    this.physicalObjects = [];
+  }
+
+  private createBox(width: number, height: number, depth: number, position: Position3D) {
+    const mesh = new THREE.Mesh(this.boxGeometry, this.material);
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+    mesh.scale.set(width, height, depth);
+    mesh.position.copy(position);
+    this.scene.add(mesh);
+
+    const shape = new CANNON.Box(new CANNON.Vec3(width * 0.5, height * 0.5, depth * 0.5));
+    const body = new CANNON.Body({
+      mass: 1,
+      position: new CANNON.Vec3(0, 3, 0),
+      shape,
+    });
+
+    body.addEventListener('collide', this.playHitSound.bind(this));
+    body.position.copy(new CANNON.Vec3(position.x, position.y, position.z));
+    this.physicsWorld.addBody(body);
+    return { mesh, physics: body };
+  }
+
+
+  private createSphere(radius: number, position: Position3D) {
+    const mesh = new THREE.Mesh(this.sphereGeometry, this.material);
+    mesh.castShadow = true;
+    mesh.receiveShadow = true;
+    mesh.scale.set(radius, radius, radius);
+    mesh.position.copy(position);
+    this.scene.add(mesh);
+
+    const shape = new CANNON.Sphere(radius);
+    const body = new CANNON.Body({ 
+      mass: 1,
+      position: new CANNON.Vec3(0, 3, 0),
+      shape,
+    });
+    body.position.copy(new CANNON.Vec3(position.x, position.y, position.z));
+    body.addEventListener('collide', this.playHitSound.bind(this));
+    this.physicsWorld.addBody(body);
+    return { mesh, physics: body };
+  }
+
+  private createFloor() {
+    const geometry = new THREE.PlaneGeometry(10, 10);
+    const material = new THREE.MeshStandardMaterial({ 
+      color: '#777777',
+      metalness: 0.3,
+      roughness: 0.4,
+      envMap: this.environmentMap,
+      envMapIntensity: 0.5
+    });
+
+    const mesh = new THREE.Mesh(geometry, material);
+    mesh.receiveShadow = true;
+    mesh.rotation.x = -Math.PI * 0.5;
+
+    const shape = new CANNON.Plane();
+    const body = new CANNON.Body();
+    body.mass  = 0;
+    body.quaternion.setFromAxisAngle(new CANNON.Vec3(-1, 0, 0), Math.PI * 0.5);
+    body.addShape(shape);
+
+    return { mesh, physics: body };
+  }
+
+  private createDirectionalLight() {
+    const light = new THREE.DirectionalLight(0xffffff, 0.6);
+    light.castShadow = true;
+    light.shadow.mapSize.set(1024, 1024);
+    light.shadow.camera.far = 15;
+    light.shadow.camera.left = - 7;
+    light.shadow.camera.top = 7;
+    light.shadow.camera.right = 7;
+    light.shadow.camera.bottom = - 7;
+    light.position.set(5, 5, 5);
+    return light;
+  }
+
+  private loadEnvironmentMap() {
+    const assetLoader = AssetLoader.getInstance();
+    const textureFolder = 'textures/environmentMap/0';
+    const urls = ['px', 'nx', 'py', 'ny', 'pz', 'nz']
+      .map(suffix => `${textureFolder}/${suffix}.png`);
+    return assetLoader.loadCubeTexture(urls);
+  }
+
   async dispose() {
     await super.dispose();
+    this.environmentMap.dispose();
+    this.floor.mesh.geometry.dispose();
+    (this.floor.mesh.material as THREE.Material).dispose();
 
+    for (const sphere of this.physicalObjects) {
+      sphere.mesh.geometry.dispose();
+      (sphere.mesh.material as THREE.Material).dispose();
+    }
   }
 }
