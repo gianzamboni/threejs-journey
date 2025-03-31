73a618bc0b926b1815985f5704d2a055665c483c
Gianfranco Zamboni <gzamboni@dc.uba.ar>
2025-03-31 11:57:01 -0300

    Tone mapping

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 
 - Transform all the colors defined in rgb to oklch
 - Make sure all the site works well when dark themed
-
+- All functions that takes an array of args could use ...args instead
 ## Temas interesantes para investigar
 https://en.wikipedia.org/wiki/Perlin_noise
 
--- src/app/journey/03-advanced-techniques/07-realistic-render.ts
+++ src/app/journey/03-advanced-techniques/07-realistic-render.ts
@@ -1,11 +1,14 @@
 import { Customizable } from "#/app/decorators/customizable";
-import { Exercise } from "#/app/decorators/exercise";
+import { Description, Exercise } from "#/app/decorators/exercise";
 import RenderView from "#/app/layout/render-view";
 import { AssetLoader, loadHelmet } from "#/app/utils/assets-loader";
 import OrbitControlledExercise from "../exercises/orbit-controlled-exercise";
 import * as THREE from 'three';
 
 @Exercise('realistic-render')
+@Description([
+  '<strong>Renderer tweaks to get a more realistic render</strong>',
+])
 export class RealisticRender extends OrbitControlledExercise {
 
   @Customizable([
@@ -22,19 +25,53 @@ export class RealisticRender extends OrbitControlledExercise {
   ])
   private _scene: THREE.Scene;
 
+  @Customizable([
+    {
+      propertyPath: 'toneMapping',
+      folderPath: 'Tone Mapping',
+      settings: {
+        name: 'Algorithm',
+        options: {
+          No: THREE.NoToneMapping,
+          Linear: THREE.LinearToneMapping,
+          Reinhard: THREE.ReinhardToneMapping,
+          Cineon: THREE.CineonToneMapping,
+          ACESFilmic: THREE.ACESFilmicToneMapping
+        }
+      }
+    }, {
+      propertyPath: 'toneMappingExposure',
+      folderPath: 'Tone Mapping',
+      settings: {
+        name: 'Exposure',
+        min: 0,
+        max: 10,
+        step: 0.001,
+      }
+    }
+  ])
+  private _renderer: THREE.WebGLRenderer;
+
   private envMap: THREE.Texture | undefined;
 
   private helmet: THREE.Object3D[] | undefined;
+
+
 
   constructor(view: RenderView) {
     super(view);
     this._scene = this.scene;
     this._scene.environmentIntensity = 1;
+    this._renderer = view.renderer;
+
     this.loadEnvironment();
     this.loadHelmet();
 
     this.camera.position.set(4, 5, 4);
     this.controls.target.y = 3.5;
+
+    this._renderer.toneMapping = THREE.ReinhardToneMapping;
+    this._renderer.toneMappingExposure = 3;
   }
 
   loadEnvironment() {
