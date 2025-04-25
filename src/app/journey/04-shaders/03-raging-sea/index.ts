b13b0d4caf83dc5c3658bdb2f9fa5f751e1859bb
Gianfranco Zamboni <gzamboni@dc.uba.ar>
2025-04-25 14:37:36 -0300

    Big waves elevation

--- src/app/journey/04-shaders/03-raging-sea/index.ts
+++ src/app/journey/04-shaders/03-raging-sea/index.ts
@@ -1,27 +1,46 @@
   import { Mesh, PlaneGeometry, ShaderMaterial } from "three";
 
+import { Customizable } from "#/app/decorators/customizable";
 import { Exercise } from "#/app/decorators/exercise";
 import RenderView from "#/app/layout/render-view";
 import { disposeMesh } from "#/app/utils/three-utils";
+import seaFragmentShader from "./sea.frag";
+import seaVertexShader from "./sea.vert";
 
 import OrbitControlledExercise from "../../exercises/orbit-controlled-exercise";
-import seaVertexShader from "./sea.vert";
-import seaFragmentShader from "./sea.frag";
+
 
 @Exercise("raging-sea")
 export class RagingSea extends OrbitControlledExercise {
 
+  @Customizable([{
+    propertyPath: "uniforms.uBigWavesElevation.value",
+    settings: {
+      min: 0,
+      max: 1,
+      step: 0.001,
+      name: "Big Waves Elevation",
+    }
+  }])
+  private material: ShaderMaterial;
+
   private water: Mesh;
+
+
 
   constructor(renderView: RenderView) {
     super(renderView);
 
     const geometry = new PlaneGeometry(2, 2, 2048, 2048);
-    const material = new ShaderMaterial({
+    this.material = new ShaderMaterial({
       vertexShader: seaVertexShader,
       fragmentShader: seaFragmentShader,
+      uniforms: {
+        uBigWavesElevation: { value: 0.2 },
+      }
     });
-    this.water = new Mesh(geometry, material);
+
+    this.water = new Mesh(geometry, this.material);
     this.water.rotation.x = -Math.PI * 0.5;
     this.scene.add(this.water);
     this.camera.position.set(1, 1, 1);
