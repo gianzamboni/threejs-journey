c4af5426d47c5547e4df0f9ab0d5740d4b32b1c9
Gianfranco Zamboni <gzamboni@dc.uba.ar>
2025-04-26 16:15:27 -0300

    Debug controller extrated  to their own files

--- src/app/journey/04-shaders/03-raging-sea/index.ts
+++ src/app/journey/04-shaders/03-raging-sea/index.ts
@@ -2,124 +2,15 @@ import { Color, Mesh, PlaneGeometry, ShaderMaterial, Vector2, Vector3 } from "th
 
 import { Timer } from 'three/addons/misc/Timer.js';
 
-import { ControllerConfig, Customizable } from "#/app/decorators/customizable";
+import { Customizable } from "#/app/decorators/customizable";
 import { Exercise } from "#/app/decorators/exercise";
 import { disposeMesh } from "#/app/utils/three-utils";
+import { RAGING_SEA_COLORS_CONTROLS, RAGING_SEA_CONTROLS } from "./controls";
 import seaFragmentShader from "./shaders/sea.frag";
 import seaVertexShader from "./shaders/sea.vert";
 
 import AnimatedExercise from "../../exercises/animated-exercise";
 
-
-const RAGING_SEA_CONTROLS: ControllerConfig[] = [{
-  propertyPath: "uniforms.uBigWavesElevation.value",
-  folderPath: "Big Waves",
-  settings: {
-    min: 0,
-    max: 10,
-    step: 0.001,
-    name: "Elevation",
-  }
-}, {
-  propertyPath: "uniforms.uBigWavesFrequency.value.x",
-  folderPath: "Big Waves",
-  settings: {
-    min: 0,
-    max: 10,
-    step: 0.001,
-    name: "Frequency X",
-  }
-}, {
-  propertyPath: "uniforms.uBigWavesFrequency.value.y",
-  folderPath: "Big Waves",
-  settings: {
-    min: 0,
-    max: 10,
-    step: 0.001,
-    name: "Frequency Y",
-  }
-}, {
-  propertyPath: "uniforms.uBigWavesSpeed.value",
-  folderPath: "Big Waves",
-  settings: {
-    min: 0,
-    max: 10,
-    step: 0.001,
-    name: "Speed",
-  }
-}, {
-  propertyPath: "uniforms.uColorOffset.value",
-  folderPath: "Colors",
-  settings: {
-    min: 0,
-    max: 1,
-    step: 0.001,
-    name: "Offset",
-  }
-}, {
-  propertyPath: "uniforms.uColorMultiplier.value",
-  folderPath: "Colors",
-  settings: {
-    min: 0,
-    max: 10,
-    step: 0.001,
-    name: "Multiplier",
-  } 
-}, {
-  propertyPath: "uniforms.uSmallWavesElevation.value",
-  folderPath: "Small Waves",
-  settings: {
-    min: 0,
-    max: 1,
-    step: 0.001,
-    name: "Elevation",
-  }
-}, {
-  propertyPath: "uniforms.uSmallWavesFrequency.value",
-  folderPath: "Small Waves",
-  settings: {
-    min: 0,
-    max: 30,
-    step: 0.001,
-    name: "Frequency",
-  }
-}, {
-  propertyPath: "uniforms.uSmallWavesSpeed.value",
-  folderPath: "Small Waves",
-  settings: {
-    min: 0,
-    max: 4,
-    step: 0.001,
-    name: "Speed",
-  }
-}, {
-  propertyPath: "uniforms.uSmallIterations.value",  
-  folderPath: "Small Waves",
-  settings: {
-    min: 0,
-    max: 5,
-    step: 1,
-    name: "Iterations",
-  }
-}]
-
-const RAGING_SEA_COLORS_CONTROLS: ControllerConfig[] = [{
-  propertyPath: "depth",
-  folderPath: "Colors",
-  type: "color",
-  settings: {
-    onChange: "onDepthColorChange",
-    name: "Depth",
-  }
-}, {
-  propertyPath: "surface",
-  folderPath: "Colors",
-  type: "color",
-  settings: {
-    onChange: "onSurfaceColorChange",
-    name: "Surface",
-  }
-}]
 
 @Exercise("sea")
 export class RagingSea extends AnimatedExercise {
