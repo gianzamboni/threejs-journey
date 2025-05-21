dc268fff24a51c21bb65f93b8963149dd3421bca
Gianfranco Zamboni <gzamboni@dc.uba.ar>
2025-05-21 15:52:49 -0300

    Clouds specular

--- src/app/journey/04-shaders/12-earth-shaders/index.ts
+++ src/app/journey/04-shaders/12-earth-shaders/index.ts
@@ -1,13 +1,17 @@
-import { Mesh, ShaderMaterial, SphereGeometry, SRGBColorSpace, Texture, Uniform } from "three";
+import { IcosahedronGeometry, Mesh, MeshBasicMaterial, ShaderMaterial, SphereGeometry, Spherical, SRGBColorSpace, Texture, Uniform, Vector3 } from "three";
 
+import { Timer } from 'three/addons/misc/Timer.js';
+
+import { Customizable } from "#/app/decorators/customizable";
+import { DebugFPS } from "#/app/decorators/debug";
 import { Description, Exercise } from "#/app/decorators/exercise";
 import OrbitControlledExercise from "#/app/journey/exercises/orbit-controlled-exercise";
+import { CustomizableMetadata } from "#/app/layout/debug-ui/controller-factory";
 import RenderView from "#/app/layout/render-view";
 import { AssetLoader } from "#/app/services/assets-loader";
 import { disposeMesh } from "#/app/utils/three-utils";
 import earthFragmentShader from "./shaders/earth.frag";
 import earthVertexShader from "./shaders/earth.vert";
-
 
 @Exercise("earth")
 @Description(
@@ -16,7 +20,29 @@ import earthVertexShader from "./shaders/earth.vert";
 )
 export class EarthShaders extends OrbitControlledExercise {
 
+  private earthMaterial: ShaderMaterial;
+
   private earth: Mesh;
+  private debugSun: Mesh;
+
+  @Customizable([{
+    propertyPath: "phi",
+    folderPath: "Sun",
+    settings: {
+      min: 0,
+      max: Math.PI,
+      onChange: "updateSun"
+    }
+  }, {
+    propertyPath: "theta",
+    folderPath: "Sun",
+    settings: {
+      min: -Math.PI,
+      max: Math.PI,
+      onChange: "updateSun"
+    }
+  }])
+  private sunSpherical: Spherical;
 
   private textures: {
     day: Texture;
@@ -27,38 +53,77 @@ export class EarthShaders extends OrbitControlledExercise {
   constructor(view: RenderView) {
     super(view);  
 
+    this.textures = this.loadTextures();
+    this.sunSpherical = new Spherical(1, Math.PI * 0.5, 0.5);
+
+    this.earthMaterial = this.createMaterial();
     const geometry = new SphereGeometry(2, 64, 64);
+    this.earth = new Mesh(geometry, this.earthMaterial);
 
-    const assetLoader = AssetLoader.getInstance();
-    this.textures = {
-      day: assetLoader.loadTexture("textures/earth/day.jpg"),
-      night: assetLoader.loadTexture("textures/earth/night.jpg"),
-      specularClouds: assetLoader.loadTexture("textures/earth/specularClouds.jpg"),
-    }
+    this.debugSun = new Mesh(
+      new IcosahedronGeometry(0.1, 2),
+      new MeshBasicMaterial()
+    );
+    this.updateSun();
 
-    this.textures.day.colorSpace = SRGBColorSpace;
-    this.textures.night.colorSpace = SRGBColorSpace;
+    this.camera.fov = 25;
+    this.camera.position.set(12, 5, 4);
+    this.camera.updateProjectionMatrix();
+    
+    this.scene.add(this.earth, this.debugSun);
+  }
 
-    const material = new ShaderMaterial({
+  @DebugFPS
+  public frame(timer: Timer): void {
+    super.frame(timer);
+    this.earthMaterial.uniforms.uTime.value = timer.getElapsed();
+  }
+
+  private createMaterial() {
+    return new ShaderMaterial({
       vertexShader: earthVertexShader,
       fragmentShader: earthFragmentShader,
       uniforms: {
         uDayTexture: new Uniform(this.textures.day),
         uNightTexture: new Uniform(this.textures.night),
         uSpecularCloudsTexture: new Uniform(this.textures.specularClouds),
-      }
+        uSunDirection: new Uniform(new Vector3(0,0,1)),
+        uTime: new Uniform(0.0)
+      } 
     });
+  }
 
-    this.earth = new Mesh(geometry, material);
-    this.camera.fov = 25;
-    this.camera.position.set(12, 5, 4);
-    this.camera.updateProjectionMatrix();
+  private loadTextures() {
+    const assetLoader = AssetLoader.getInstance();
+    const textures = {
+      day: assetLoader.loadTexture("textures/earth/day.jpg"),
+      night: assetLoader.loadTexture("textures/earth/night.jpg"),
+      specularClouds: assetLoader.loadTexture("textures/earth/specularClouds.jpg"),
+    }
+
+    textures.day.colorSpace = SRGBColorSpace;
+    textures.night.colorSpace = SRGBColorSpace;
     
-    this.scene.add(this.earth);
+    textures.day.anisotropy = this.view.renderer.capabilities.getMaxAnisotropy();
+    textures.night.anisotropy = this.view.renderer.capabilities.getMaxAnisotropy();
+    
+    return textures;
+  }
+
+  private updateSun(newValue?: number, customizableMetadata?: CustomizableMetadata) {
+    if(customizableMetadata && newValue) {
+      this.sunSpherical[customizableMetadata.property as ('phi' | 'theta')] = newValue;
+    }
+
+    const sunDirection = new Vector3();
+    sunDirection.setFromSpherical(this.sunSpherical);
+    this.debugSun.position.copy(sunDirection).multiplyScalar(5);
+    this.earthMaterial.uniforms.uSunDirection.value.copy(sunDirection);
   }
 
   async dispose() {
     super.dispose();
     disposeMesh(this.earth);
+    disposeMesh(this.debugSun);
   }
 }
--- src/app/journey/04-shaders/12-earth-shaders/shaders/earth.frag
+++ src/app/journey/04-shaders/12-earth-shaders/shaders/earth.frag
@@ -1,19 +1,30 @@
 uniform sampler2D uDayTexture;
 uniform sampler2D uNightTexture;
 uniform sampler2D uSpecularCloudsTexture;
-
+uniform vec3 uSunDirection;
+uniform float uTime;
 varying vec2 vUv;
 varying vec3 vNormal;
 varying vec3 vPosition;
 
-void main()
-{
-    vec3 viewDirection = normalize(vPosition - cameraPosition);
-    vec3 normal = normalize(vNormal);
-    vec3 color = vec3(vUv, 1.0);
 
-    // Final color
-    gl_FragColor = vec4(color, 1.0);
-    #include <tonemapping_fragment>
-    #include <colorspace_fragment>
+void main() {
+  vec3 viewDirection = normalize(vPosition - cameraPosition);
+  vec3 normal = normalize(vNormal);
+
+  vec3 dayColor = texture(uDayTexture, vUv).rgb;
+  vec3 nightColor = texture(uNightTexture, vUv).rgb;
+
+  float sunOrientation = dot(uSunDirection, normal); 
+  float dayMix = smoothstep(-0.25, 0.5, sunOrientation);
+  vec3 color = mix(nightColor, dayColor, dayMix);
+
+  vec2 uvXTranslation = vec2(mod(vUv.x + uTime * 0.001, 1.0), vUv.y);
+  vec2 specularCloudColor = texture(uSpecularCloudsTexture, uvXTranslation).rg;
+  float cloudMix = smoothstep(0.0, 1.0, specularCloudColor.g / 2.0) * dayMix ;
+  color = mix(color, vec3(1.0), cloudMix);
+
+  gl_FragColor = vec4(color, 1.0);
+  #include <tonemapping_fragment>
+  #include <colorspace_fragment>
 }
